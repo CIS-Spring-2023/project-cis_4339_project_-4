caa19384a1d9028c01f851fd083b517222dbe92d
Phat Vo <pmvo2@cougarnet.uh.edu>
2023-03-04 14:58:12 -0600

    Update function: Add new service and list service detail, still working on edit services

---
+++ frontend/src/components/editservice.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data(){
+    return{
+    selectedData:null
+    }
+  },
+  mounted() {
+    const serviceid = this.$route.params.id // get the ID from the URL
+          const storedData = JSON.parse(localStorage.getItem(key));
+          console.log(storedData)
+          const selectedData = storedData.find(item => item.id === serviceid); 
+          if(selectedData)
+          {            
+            this.selectedData = selectedData
+          }
+  }
+}
+</script>
+<template>
+<li>testing</li>
+</template>
