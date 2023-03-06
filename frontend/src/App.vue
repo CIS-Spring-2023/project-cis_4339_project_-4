0704b741573fa934168513fed98035fc0011fc1c
dmdiallo <78002032+dmdiallo@users.noreply.github.com>
2023-03-05 22:07:43 -0600

    Add files via upload

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -75,6 +75,16 @@ export default {
                 Find Event
               </router-link>
             </li>
+            <li>
+              <router-link to="/findservices">
+                <span
+                  style="position: relative; top: 6px"
+                  class="material-icons"
+                  >search</span
+                >
+                Find Service
+              </router-link>
+            </li>
           </ul>
         </nav>
       </header>
